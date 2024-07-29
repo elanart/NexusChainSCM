df55da371b603153d5659e22ef68d670c3887c8e
Tran Le Lan <elanart.06@gmail.com>
2024-07-29 22:11:30 +0700

    init project

---
+++ nexuschain/src/main/java/com/tll/nexuschain/NexuschainApplication.java
@@ -0,0 +1,13 @@
+package com.tll.nexuschain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuschainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuschainApplication.class, args);
+	}
+
+}
---
+++ nexuschain/src/test/java/com/tll/nexuschain/NexuschainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tll.nexuschain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuschainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
